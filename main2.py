2a4708b06c65b49791fe97d20723e32a0e1fb4ec
Ray Berger <RayBB@users.noreply.github.com>
2021-08-04 16:48:16 -0400

    dability to pull multiple weeks, deal with monthly lists

--- main2.py
+++ main2.py
@@ -2,26 +2,44 @@ import click
 import requests
 import json
 import os
+from time import sleep
+from datetime import date, datetime, timedelta
 
 API_KEY = os.getenv('NYT_API_KEY')
+HOST = "https://api.nytimes.com"
 
-def make_api_request(url, additional_params):
-    params_with_key = {
-        'api-key': API_KEY
-    }
+def make_api_request(url, additional_params, retries=10):
+    params_with_key = {'api-key': API_KEY}
     params_with_key.update(additional_params)
     r = requests.get(url, params=params_with_key)
-    return r.json()
+    if r.status_code == 200:
+        return r.json()
+    if r.status_code == 429 and retries > 0:
+        sleep(6)
+        return make_api_request(url, additional_params, retries - 1)
+    
+    print("Request failed", additional_params, url)
+    print(r.headers)
+    print(r.json())
+    raise Exception("Request failed")
 
-def get_overview(published_date: str):
-    url = "https://api.nytimes.com/svc/books/v3/lists/full-overview.json"
-    return  make_api_request(url, {'published_date': published_date})
+def get_overview(published_date: datetime):
+    url = HOST + "/svc/books/v3/lists/full-overview.json"
+    return  make_api_request(url, {'published_date': date_to_str(published_date)})
 
-def process_overview_response(response: dict):
+def first_time_on_list(list, book):
+    # monthly books have a lowest weeks_on_list of 0
+    # weekly  books have a lowest weeks_on_list of 1
+    # This is an error in NYT API, in future monthly books may have weeks_on_list of 1 as a minimum
+    if list.get("updated") == "WEEKLY":
+        return book.get('weeks_on_list') == 1
+    if list.get("updated") == "MONTHLY":
+        return book.get('weeks_on_list') == 0
+
+def process_overview_response(response: dict, seen_isbns: set):
     outputs = []
     published_date = response.get('results').get('published_date')
     print(f"Processing date: {published_date}")
-    print(len(response.get('results').get('lists')), "lists")
     for list in response.get('results').get('lists'):
         list_data = {
             'list_name_encoded': list['list_name_encoded'],
@@ -30,11 +48,14 @@ def process_overview_response(response: dict):
         }
         for book in list.get('books'):
             # only get books that are on the list for the first time
-            if (book['weeks_on_list'] == 0):
+            if first_time_on_list(list, book):
                 # we prefer isbn 13 but if that is not available we will use isbn 10
                 isbn = book.get('primary_isbn13', "")
                 isbn = isbn if len(isbn) == 13 else book['primary_isbn10']
-                list_data['isbns'].append(isbn)
+                # we need to track seen isbns because monthly lists don't update "weeks_on_list" each week
+                if isbn not in seen_isbns:
+                    list_data['isbns'].append(isbn)
+                    seen_isbns.add(isbn)
         outputs.append(list_data)
     print(sum([len(x.get('isbns')) for x in outputs]), "new books")
     return outputs
@@ -44,13 +65,26 @@ def write_result_to_disk(outputs, output_file: str):
         json.dump(outputs, fp)
     print(f"results written to {output_file}")
 
+def date_to_str(date: datetime):
+    return date.strftime('%Y-%m-%d')
+
 @click.command()
 @click.option('--output_file', default="result.json", help='Where to write the results')
-@click.option('--published_date', default="", help='Date in YYYY-MM-DD format to add books from. Defaults to today.')
-def run_with_click(output_file, published_date):
-    overview = get_overview(published_date)
-    outputs = process_overview_response(overview)
+@click.option('--date-start', type=click.DateTime(formats=["%Y-%m-%d"]),
+              default=str(date.today()), help='Date to start adding books from. Defaults to today.')
+@click.option('--date-end', type=click.DateTime(formats=["%Y-%m-%d"]),
+              default=str(date.today()), help='Date to stop adding books from. Defaults to today.')
+def run_with_click(output_file, date_start, date_end):
+    current_date = date_start + timedelta(days=0)
+    outputs = []
+    seen_isbns = set()
+    while current_date <= date_end:
+        overview = get_overview(current_date)
+        outputs.extend(process_overview_response(overview, seen_isbns))
+        current_date = current_date + timedelta(days=7)
+
     write_result_to_disk(outputs, output_file)
+    print(f"{len(seen_isbns)} total books found between {date_start} and {date_end}")
 
 if __name__ == "__main__":
     run_with_click()
