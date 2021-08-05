a6c9e443511dedd165f527d1fdb63a6ec9f2d411
Alex Korobko <korobko.alex@gmail.com>
2021-08-04 20:10:34 -0400

    Added new job to add nyt review links.

--- add_nyt_review_link.py
+++ add_nyt_review_link.py
@@ -1,23 +1,18 @@
 """
-A bot to add a NYT bestseller tags 'nyt:{list_name_encoded}={published_date}'
-and 'New York Times bestseller' to each book from the list of ISBNs.
+A bot to add a NYT review link to the links array for a work
+ {'title': '"It\'s a steal" (NYT review)',
+   'url': 'https://www.nyt.com/books/2007/jan/06/featuresreviews',
+   'type': {'key': '/type/link'}}
+ each book from the list of ISBNs.
 
-The list of bestsellers to add is a json file of the following format:
+The list of reviews to add is a json file of the following format:
 [
     {
-        "list_name_encoded": "business-books",
-        "published_date": "2020-07-12",
-        "isbns": [
-            "9780735211292",
-            "9780399592522"
-        ]
     },
 ]
 This script would be called from the command line like so:
-$ python add_nyt_bestseller_tag.py --file=~/nyt_bstslrs.json
---limit=1 --dry-run=True
-NOTE: This script checks if there is a tag on a book that starts with
-'nyt:' and if the book has such a tag the script does nothing.
+$ python add_nyt_review.py --file=~/nyt_reviews.json --dry-run=True
+NOTE: This script checks if there is a link in the links with the same URL
 If there is no such book with requested isbn in the OL, the script makes request
 by the book isbn https://openlibrary.org/isbn/{isbn} , triggering auto import
 """
@@ -28,41 +23,38 @@ import requests
 from olclient.bots import AbstractBotJob
 
 
-class AddNytBestsellerJob(AbstractBotJob):
-    NYT_TAG_PREFIX = 'nyt:'
-    NYT_TAG_BESTSELLER = 'New York Times bestseller'
+class AddNytReviewJob(AbstractBotJob):
 
     def __init__(self):
-        super().__init__(job_name='AddNytBestseller')
+        super().__init__(job_name='AddNytReviewJob')
 
-    def need_to_add_nyt_bestseller_tag(self, work) -> bool:
+    def need_to_add_nyt_review_link(self, work, link) -> bool:
         """Returns False if the book already has
-        a tag that starts with 'nyt:'"""
+        the same link in the links section"""
         try:
-            for subj in work.subjects:
-                if subj.startswith(
-                    (self.NYT_TAG_PREFIX, self.NYT_TAG_BESTSELLER)):
+            for lnk in work.links:
+                if lnk.url.startswith(link):
                     return False
             return True
         except AttributeError:
             self.logger.info(
-                'Failed to check subjects for work {}, no subject list exist'
+                'Failed to check link for work {}, no links list exist'
                 .format(work.olid))
             return True
 
-    def add_tags(self, work, new_tags) -> None:
-        """Adds a new tag to a work"""
+    def add_link(self, work, link_struct) -> None:
+        """Adds a new link to a work"""
         try:
-            work.subjects.extend(new_tags)
+            work.links.extend(link_struct)
             self.logger.info(
-                'Successfully extended new NYT tags to work {}'.format(
-                    work.olid))
+                'Successfully extended new link with NYT rewiew to work {}'
+                    .format(work.olid))
         except AttributeError:
-            work.subjects = new_tags
+            work.subjects = [link_struct]
             self.logger.info(
-                'Failed to append subjects list for work {} but '
-                'SUCCESSFULLY CREATED new subjects list with NYT tags'.format(
-                    work.olid))
+                'Failed to append links list for work {} but '
+                'SUCCESSFULLY CREATED new links list with NYT review'
+                    .format(work.olid))
 
     def request_book_import_by_isbn(self, book_isbn) -> None:
         """ Makes request to the book_isbn
@@ -77,27 +69,28 @@ class AddNytBestsellerJob(AbstractBotJob):
 
     def save_job_results(self, job_results) -> None:
         self.logger.info('Job execution results: {}'.format(repr(job_results)))
-        with open('add_nyt_bestseller_result.json', 'w', encoding='utf-8') as f:
+        with open('add_nyt_review_result.json', 'w', encoding='utf-8') as f:
             json.dump(job_results, f, ensure_ascii=False, indent=4)
 
     def process_found_bestseller_edition(self, bstslr_record_isbn,
-        bstslr_edition, new_tags, comment, job_results) -> None:
-        if self.need_to_add_nyt_bestseller_tag(bstslr_edition.work):
+        bstslr_edition, link_struct, comment, job_results) -> None:
+        if self.need_to_add_nyt_review_link(bstslr_edition.work):
             self.logger.info(
-                'The NYT tags to be added for the work {} of the edition {}'
+                'The NYT review link to be added '
+                'for the work {} of the edition {}'
                     .format(bstslr_edition.work.olid,
                             bstslr_record_isbn))
-            self.add_tags(bstslr_edition.work, new_tags)
+            self.add_link(bstslr_edition.work, link_struct)
             bstslr_edition.work.save(comment)
             bstslr_edition.save(comment)
-            job_results['tags_added'] = job_results['tags_added'] + 1
+            job_results['links_added'] = job_results['links_added'] + 1
         else:
             self.logger.info(
-                'A NYT tag already exists for the work {}'
+                'A NYT link already exists for the work {}'
                 ' of the edition {}, skipping'
                     .format(bstslr_edition.work.olid, bstslr_record_isbn))
-            job_results['tags_already_exist'] = \
-                job_results['tags_already_exist'] + 1
+            job_results['links_already_exist'] = \
+                job_results['links_already_exist'] + 1
 
     def process_bestseller_group_record(self, bestseller_group_record, comment,
         job_results) -> None:
@@ -130,9 +123,9 @@ class AddNytBestsellerJob(AbstractBotJob):
     def run(self) -> None:  # overwrites the AbstractBotJob run method
         self.dry_run_declaration()
         job_results = {'input_file': self.args.file, 'books_imported': 0,
-                       'tags_added': 0, 'tags_already_exist': 0,
+                       'links_added': 0, 'links_already_exist': 0,
                        'isbns_failed': 0, 'dry_run': self.dry_run}
-        comment = 'Add NYT bestseller tag'
+        comment = 'Add NYT review links'
         with open(self.args.file, 'r') as fin:
             bestsellers_data = json.load(fin)
             for bestseller_group_record in bestsellers_data:
@@ -142,10 +135,10 @@ class AddNytBestsellerJob(AbstractBotJob):
 
 
 if __name__ == "__main__":
-    job = AddNytBestsellerJob()
+    job = AddNytReviewJob()
 
     try:
         job.run()
     except Exception as e:
-        job.logger.exception("The AddNytBestsellerJob thrown an exception")
+        job.logger.exception("The AddNytReviewJob thrown an exception")
         raise e
