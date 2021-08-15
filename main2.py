d232ac80d54ac718050dc46169b33c6413183fca
Ray Berger <RayBB@users.noreply.github.com>
2021-08-15 11:49:58 -0400

    find reviews and save

--- main2.py
+++ main2.py
@@ -36,6 +36,28 @@ def first_time_on_list(list, book):
     if list.get("updated") == "MONTHLY":
         return book.get('weeks_on_list') == 0
 
+def process_overview_response_reviews(response):
+    """
+    Get all the reviews from an overview response
+    """
+    outputs = {}
+    for list in response.get('results').get('lists'):
+        for book in list.get('books'):
+            isbn = book.get('primary_isbn13', "")
+            isbn = isbn if len(isbn) == 13 else book['primary_isbn10']
+            reviews = set(outputs.get(isbn, {}).get('reviews', []))
+
+            if book.get('book_review_link'):
+                reviews.add(book.get('book_review_link'))
+            if book.get('sunday_review_link'):
+                reviews.add(book.get('sunday_review_link'))
+
+            if len(reviews) > 0:
+                # we convert back to a list because we need to serialize
+                outputs[isbn] = {'reviews': [x for x in reviews], 'isbn': isbn}
+    return outputs
+            
+
 def process_overview_response(response: dict, seen_isbns: set):
     outputs = []
     published_date = response.get('results').get('published_date')
@@ -77,13 +99,16 @@ def date_to_str(date: datetime):
 def run_with_click(output_file, date_start, date_end):
     current_date = date_start + timedelta(days=0)
     outputs = []
+    reviews = {}
     seen_isbns = set()
     while current_date <= date_end:
         overview = get_overview(current_date)
         outputs.extend(process_overview_response(overview, seen_isbns))
+        reviews.update(process_overview_response_reviews(overview))
         current_date = current_date + timedelta(days=7)
 
     write_result_to_disk(outputs, output_file)
+    write_result_to_disk(reviews, 'reviews.json')
     print(f"{len(seen_isbns)} total books found between {date_start} and {date_end}")
 
 if __name__ == "__main__":
